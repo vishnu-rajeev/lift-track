92620bc5cba030390d9f6d2089d6bbb145cf3bdc
Vishnu Rajeev <vishrajeev94@gmail.com>
2026-07-26 21:31:34 +0200

    feat: add exercise repository

---
+++ src/main/java/com/vishnurajeev/lifttrack/exercise/ExerciseRepository.java
@@ -0,0 +1,6 @@
+package com.vishnurajeev.lifttrack.exercise;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ExerciseRepository extends JpaRepository<Exercise, Long> {
+}
